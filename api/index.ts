30557c2a777968cea8ae75e2123bbc581152dc8e
BennysonSamuel <bennyson64@gmail.com>
2026-02-02 15:08:47 +0530

    check

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,10 @@ export const config = {
   runtime: "nodejs",
 };
 
-import { app } from "../src/app.js";
+// import { app } from "../src/app.js";
 
-export default app;
+// export default app;
+
+export default function handler() {
+  return new Response("Node runtime runs");
+}
